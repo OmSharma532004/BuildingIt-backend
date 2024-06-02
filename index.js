c3c76aa3411a3396c7eb5944d6821d6f892d8c1e
OmSharma532004 <141063669+OmSharma532004@users.noreply.github.com>
2024-06-02 18:23:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ require('./passport.js');
 app.use(fileUpload());
 app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:5175','http://localhost:5174','http://localhost:5173'], // Ensure this matches your frontend URL
+  origin: '*',// Ensure this matches your frontend URL
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type']
 }));
